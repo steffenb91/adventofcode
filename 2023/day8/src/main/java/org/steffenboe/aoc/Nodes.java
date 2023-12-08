3bab9ffa86ee994787efb55807faa58f66698aab
steffenboe <steffenb1991.8@gmail.com>
2023-12-08 23:01:51 +0100

    solution day 2

--- 2023/day8/src/main/java/org/steffenboe/aoc/Nodes.java
+++ 2023/day8/src/main/java/org/steffenboe/aoc/Nodes.java
@@ -1,6 +1,8 @@
 package org.steffenboe.aoc;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -8,37 +10,42 @@ import java.util.stream.Stream;
 
 record Nodes(String instructions, Map<String, Node> nodes) {
 
-    public long steps(List<String> start) throws InterruptedException {
-        long steps1 = 0;
-        // für jede Richtung
-        // neuen Thread pro next Knoten
-        // alle threads speichern ihren current und next knoten ab
-        // sind alle Knoten auf Z?
-        // falls ja, beende und gebe steps zurück
-        // falls nein, beginne neue Richtung
-        Stream<Character> infiniteStream = Stream.iterate(0, i -> (i + 1) % instructions.toCharArray().length)
-                .map(i -> instructions.toCharArray()[i]);
-        Iterator<Character> iterator = infiniteStream.iterator();
-        List<String> current = new ArrayList<>(start);
-        while (iterator.hasNext()) {
-            List<String> next = new ArrayList<>();
-            char instruction = iterator.next();
-            for (String node : current) {
-                Node currentNode = nodes.get(node);
+    /**
+     * from reddit: Part 2 you have to be a bit clever since you can't simulate everything as it
+     * will take too long. The trick is to simulate each start independently until
+     * it first hits a node ending in "Z", then take the LCM of all the steps. It
+     * assumes each start hits a target node in a cyclic manner, which seems to be
+     * true.
+     */
+    public BigInteger steps(List<String> start) throws InterruptedException {
+        Map<String, BigInteger> stepsForEachNode = new HashMap<>();
+        for (String node : start) {
+            BigInteger steps1 = new BigInteger("0");
+            Stream<Character> infiniteStream = Stream.iterate(0, i -> (i + 1) % instructions.toCharArray().length)
+                    .map(i -> instructions.toCharArray()[i]);
+            Iterator<Character> iterator = infiniteStream.iterator();
+            String next = node;
+            while (iterator.hasNext()) {
+                char instruction = iterator.next();
+
+                Node currentNode = nodes.get(next);
                 if (instruction == 'L') {
-                    next.add(currentNode.left());
+                    next = currentNode.left();
                 } else {
-                    next.add(currentNode.right());
+                    next = currentNode.right();
                 }
-            }
-            steps1++;
-            current = next;
-            if (current.stream().parallel().allMatch(id -> id.endsWith("Z"))) {
-                return steps1;
+                steps1 = steps1.add(new BigInteger("1"));
+                if (next.endsWith("Z")) {
+                    System.out.println("Found end for " + node + ": " + next);
+                    stepsForEachNode.put(node, steps1);
+                    break;
+                }
             }
         }
 
-        return steps1;
+        // lcm calculation
+        return stepsForEachNode.values().stream()
+                .reduce(BigInteger.ONE, (a, b) -> a.multiply(b).divide(a.gcd(b)));
 
     }
 
